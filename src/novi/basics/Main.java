43353cb03779054089f4d3999f4336de83693106
Joey Hasselbach <joey_h1988@live.nl>
2020-06-02 11:35:41 +0200

    logincontroller

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -3,6 +3,7 @@ package novi.basics;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Joey");
+        LoginController loginController = new LoginController();
     }
 }
